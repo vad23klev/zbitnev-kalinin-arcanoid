2ef00bd0a69bde9e5a2a5261c68b7abf734ed874
Nikita Kalinin <nixorv@gmail.com>
2014-10-22 17:51:29 +0400

    Создан проект.

---
+++ src/ArkanoidLoader.java
@@ -0,0 +1,14 @@
+import com.golden.gamedev.GameLoader;
+
+
+public class ArkanoidLoader extends GameLoader {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
