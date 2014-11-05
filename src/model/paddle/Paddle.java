2e71862be514f2c495f3e467f5c79e1771d58bd7
Nikita Kalinin <nixorv@gmail.com>
2014-11-05 15:47:46 +0300

    Добавлена заготовка класса Paddle

---
+++ src/model/paddle/Paddle.java
@@ -0,0 +1,12 @@
+package model.paddle;
+
+import model.IngameObject;
+
+/**
+ * Модель абстрактной ракетки.
+ * @author Nikita Kalinin <nixorv@gmail.com>
+ *
+ */
+public abstract class Paddle extends IngameObject {
+
+}
