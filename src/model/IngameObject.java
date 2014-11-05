9e97d06707b2aa88177df81e579f0bb40d94cce4
Gregory Zbitnev <zbitnev@hotmail.com>
2014-11-05 16:40:09 +0300

    SpeedChangeListener реализован для классов игрового объекта и его представления

--- src/model/IngameObject.java
+++ src/model/IngameObject.java
@@ -1,17 +1,25 @@
 package model;
 
 import java.awt.geom.Point2D.Float;
+
 import model.interaction.PositionChangeListener;
+import model.interaction.SpeedChangeListener;
 
 /**
  * Класс игрового объекта.
  * @author Nikita Kalinin <nixorv@gmail.com>
  *
  */
-public class IngameObject implements PositionChangeListener {
+public class IngameObject implements PositionChangeListener, SpeedChangeListener {
 
 	@Override
 	public void positionChanged(Float newpos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void speedChanged(Speed2D newspeed) {
 		// TODO Auto-generated method stub
 		
 	}
--- src/view/IngameObjectView.java
+++ src/view/IngameObjectView.java
@@ -1,17 +1,26 @@
 ﻿package view;
 
 import java.awt.geom.Point2D.Float;
+
+import model.Speed2D;
 import model.interaction.PositionChangeListener;
+import model.interaction.SpeedChangeListener;
 
 /**
  * Представление отдельного игрового объекта
  * @author Gregory Zbitnev <zbitnev@hotmail.com>
  *
  */
-public class IngameObjectView implements PositionChangeListener {
+public class IngameObjectView implements PositionChangeListener, SpeedChangeListener {
 
 	@Override
 	public void positionChanged(Float newpos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void speedChanged(Speed2D newspeed) {
 		// TODO Auto-generated method stub
 		
 	}
