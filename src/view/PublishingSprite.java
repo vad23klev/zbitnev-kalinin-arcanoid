c5d0c73bf76ca4f510dc2b6c27abb38973b4eb18
Gregory Zbitnev <zbitnev@hotmail.com>
2014-11-05 15:07:06 +0300

    Добавлена заготовка класса PublishingSprite.java

---
+++ src/view/PublishingSprite.java
@@ -0,0 +1,12 @@
+﻿package view;
+
+import com.golden.gamedev.object.Sprite;
+
+/**
+ * Спрайт, знающий о том, частью какого игрового объекта (IngameObjectView) он является
+ * @author Gregory Zbitnev <zbitnev@hotmail.com>
+ *
+ */
+public class PublishingSprite extends Sprite {
+
+}
