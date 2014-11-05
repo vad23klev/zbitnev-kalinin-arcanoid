bca14c1de2c37f0d426c771fe4e98c37d68a28f6
Gregory Zbitnev <zbitnev@hotmail.com>
2014-11-05 15:05:50 +0300

    Добавлена заготовка класса ArkanoidPlayField

---
+++ src/view/ArkanoidPlayField.java
@@ -0,0 +1,13 @@
+﻿package view;
+
+import com.golden.gamedev.object.PlayField;
+
+/**
+ * Игровое поле арканоида. Содержит все обекты игры, ответственнен за обновление, рендеринг и
+ * проверку стоклновений 
+ * @author Gregory Zbitnev <zbitnev@hotmail.com>
+ *
+ */
+public class ArkanoidPlayField extends PlayField {
+
+}
