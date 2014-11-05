369b8b73748272f815a68eedfd066e96422d52b3
Gregory Zbitnev <zbitnev@hotmail.com>
2014-11-05 15:06:16 +0300

    Добавлена заготовка класса GameView

---
+++ src/view/GameView.java
@@ -0,0 +1,19 @@
+﻿package view;
+
+import com.golden.gamedev.GameEngine;
+import com.golden.gamedev.GameObject;
+
+/**
+ * Класс визуального представления игры
+ * @author Gregory Zbitnev <zbitnev@hotmail.com>
+ *
+ */
+public class GameView extends GameEngine {
+
+	@Override
+	public GameObject getGame(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
