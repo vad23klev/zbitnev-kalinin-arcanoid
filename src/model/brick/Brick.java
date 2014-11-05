091974310b4dd669618a3ad5cf89f674b48a1451
Nikita Kalinin <nixorv@gmail.com>
2014-11-05 15:31:29 +0300

    Добавлена заготовка класса Brick

---
+++ src/model/brick/Brick.java
@@ -0,0 +1,12 @@
+package model.brick;
+
+import model.IngameObject;
+
+/**
+ * Модель абстрактного кирпича.
+ * @author Nikita Kalinin <nixorv@gmail.com>
+ *
+ */
+public abstract class Brick extends IngameObject {
+
+}
