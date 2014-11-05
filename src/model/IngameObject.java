30234699c8c348fca4210ab48ff258b8267b8ee4
Gregory Zbitnev <zbitnev@hotmail.com>
2014-11-05 16:32:26 +0300

    Интерфейс PositionChangeListener реализовал для классов игрового объекта и его представления

--- src/model/IngameObject.java
+++ src/model/IngameObject.java
@@ -1,10 +1,19 @@
 package model;
 
+import java.awt.geom.Point2D.Float;
+import model.interaction.PositionChangeListener;
+
 /**
  * Класс игрового объекта.
  * @author Nikita Kalinin <nixorv@gmail.com>
  *
  */
-public class IngameObject {
+public class IngameObject implements PositionChangeListener {
+
+	@Override
+	public void positionChanged(Float newpos) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/view/IngameObjectView.java
+++ src/view/IngameObjectView.java
@@ -1,10 +1,19 @@
 ﻿package view;
 
+import java.awt.geom.Point2D.Float;
+import model.interaction.PositionChangeListener;
+
 /**
  * Представление отдельного игрового объекта
  * @author Gregory Zbitnev <zbitnev@hotmail.com>
  *
  */
-public class IngameObjectView {
+public class IngameObjectView implements PositionChangeListener {
+
+	@Override
+	public void positionChanged(Float newpos) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
