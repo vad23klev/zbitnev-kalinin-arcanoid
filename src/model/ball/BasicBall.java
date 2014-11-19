1365607907ee14ca84e87cb9aae3f2dedcb9a5c6
Nikita Kalinin <nixorv@gmail.com>
2014-11-19 16:38:15 +0300

    Добавлен интерфейс CanBeInSwarm.

--- src/model/ball/BasicBall.java
+++ src/model/ball/BasicBall.java
@@ -1,10 +1,12 @@
 package model.ball;
 
+import model.swarm.CanBeInSwarm;
+
 /**
  * Модель обычного шарика
  * @author Gregory Zbitnev <zbitnev@hotmail.com>
  *
  */
-public class BasicBall extends Ball {
+public class BasicBall extends Ball implements CanBeInSwarm {
 
 }
