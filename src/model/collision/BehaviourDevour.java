8aecc963cf0ac2d73f99e7ee1f2bac6446a293c9
Nikita Kalinin <nixorv@gmail.com>
2014-11-19 16:32:24 +0300

    Добавлен класс поведения поглощения

---
+++ src/model/collision/BehaviourDevour.java
@@ -0,0 +1,36 @@
+package model.collision;
+
+import model.IngameObject;
+
+/**
+ * Поведение поглощения при столкновении. 
+ * @author Nikita Kalinin <nixorv@gmail.com>
+ *
+ */
+public class BehaviourDevour extends CollisionBehaviour {
+	/**
+	 * Экзмепляр синглтона.
+	 */
+	private static BehaviourDevour instance = null;
+	
+	protected BehaviourDevour() {
+	}
+	
+	/**
+	 * Возвращает экземпляр поведения поглощения.
+	 * @return
+	 */
+	public static BehaviourDevour getInstance() {
+		
+		if (instance == null) {
+			instance = new BehaviourDevour();
+		}
+		
+		return instance;
+	}
+	
+	@Override
+	public void invoke(IngameObject from, IngameObject to) {
+		// TODO
+	}
+}
