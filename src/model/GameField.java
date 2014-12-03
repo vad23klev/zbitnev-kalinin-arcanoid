ded2ff429b65a47452caf7503dd9fd26557d4df8
Gregory Zbitnev <zbitnev@hotmail.com>
2014-12-03 14:59:22 +0300

    Добавлены геттеры/сеттеры игровых объектов

--- src/model/GameField.java
+++ src/model/GameField.java
@@ -7,4 +7,19 @@ package model;
  */
 public class GameField {
 
+	/**
+	 * Добавить объект на поле
+	 * @param object Объект для добавления
+	 */
+	public void addObject(IngameObject object) {
+		
+	}
+	
+	/**
+	 * Убрать объект с поля
+	 * @param object Объект для удаления
+	 */
+	public void removeObject(IngameObject object) {
+		
+	}
 }
