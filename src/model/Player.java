81a699fb7fb1afc1b9995dffc2ff2d147f1e354a
Gregory Zbitnev <zbitnev@hotmail.com>
2014-12-03 15:25:52 +0300

    Классу игрока доваблены методы управления списком ракеток

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.util.ArrayList;
+
+import model.paddle.Paddle;
+
 /**
  * Модель игрока, управляющего ракеткой.
  * @author Gregory Zbitnev <zbitnev@hotmail.com>
@@ -7,4 +11,41 @@ package model;
  */
 public class Player {
 
+	/**
+	 * Инициализировать игрока
+	 * @param paddle Подконтрольная игроку ракетка
+	 */
+	public Player(Paddle paddle) {
+		
+	}
+	
+	public Player() {
+		
+	}
+
+	/**
+	 * Получить контролируемые ракетки
+	 * @return Список контролируемых ракеток
+	 */
+	public ArrayList<Paddle> getPaddles() {
+		
+		return null;
+	}
+	
+	/**
+	 * Добавить ракетку под контроль игрока
+	 * @param paddle Ракетка для добавления
+	 */
+	public void addPaddle(Paddle paddle) {
+		
+	}
+	
+	/**
+	 * Убрать ракетку из-под контроля игрока
+	 * @param paddle Ракетка
+	 */
+	public void removePaddle(Paddle paddle) {
+		
+	}
+	
 }
