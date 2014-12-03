e7e722845223f9bb1857293d129461edb1a8154a
Gregory Zbitnev <zbitnev@hotmail.com>
2014-12-03 14:37:40 +0300

    Добавлены классы для режимов игры и игрового меню

---
+++ src/view/ScreenMenu.java
@@ -0,0 +1,38 @@
+package view;
+
+import java.awt.Graphics2D;
+
+import com.golden.gamedev.GameEngine;
+import com.golden.gamedev.GameObject;
+
+/**
+ * Режим главного игрового меню
+ * @author Gregory Zbitnev <zbitnev@hotmail.com>
+ *
+ */
+public class ScreenMenu extends GameObject {
+
+	public ScreenMenu(GameEngine arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initResources() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(Graphics2D arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(long arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
