c47b492c60c780d0e8e27be3a0fb669d6e847776
Gregory Zbitnev <zbitnev@hotmail.com>
2015-01-15 12:30:32 +0300

    Добавлена прорисовка фона игры

--- src/view/ScreenGame.java
+++ src/view/ScreenGame.java
@@ -1,9 +1,19 @@
 package view;
 
+import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import model.GameField;
+import model.GameModel;
+import model.ball.BasicBall;
 
 import com.golden.gamedev.GameEngine;
 import com.golden.gamedev.GameObject;
+import com.golden.gamedev.object.Background;
+import com.golden.gamedev.object.SpriteGroup;
+import com.golden.gamedev.object.background.ImageBackground;
 
 /**
  * Режим игры
@@ -11,6 +21,16 @@ import com.golden.gamedev.GameObject;
  *
  */
 public class ScreenGame extends GameObject {
+	
+	GameModel model;
+	
+	Background background;
+	BufferedImage basicBallImg;
+	BufferedImage breakableBrickImg;
+	
+	SpriteGroup spritesBalls;
+	SpriteGroup spritesBricks;
+	SpriteGroup spritesPaddles;
 
 	public ScreenGame(GameEngine arg0) {
 		super(arg0);
@@ -19,20 +39,50 @@ public class ScreenGame extends GameObject {
 
 	@Override
 	public void initResources() {
-		// TODO Auto-generated method stub
-
+		
+		// Загрузка фона
+		Image bgimage = bsLoader.getImage("images/bg-blue.png");
+		BufferedImage bufferedBgImage = new BufferedImage(this.getWidth(), this.getHeight(), 
+														  BufferedImage.TYPE_4BYTE_ABGR);
+		bufferedBgImage.getGraphics().drawImage(bgimage, 0, 0, this.getWidth(), 
+												this.getHeight(), null);
+		background = new ImageBackground(bufferedBgImage);
+		
+		// Загрузка изображений для игровых объектов
+		basicBallImg = bsLoader.getImage("images/ball32.png");
+		breakableBrickImg = bsLoader.getImage("images/brick_24x48.png");
+		spritesBalls = new SpriteGroup("Balls");
+		spritesBricks = new SpriteGroup("Bricks");
+		spritesPaddles = new SpriteGroup("Paddles");
+		
+		// Загрузка игрового уровня
+		// TODO: Загрузка уровня из файла (пока уровень захардкоден)
+		// TODO: Сообщить окну о габаритах уровня, чтобы то адаптировалось
+		GameField field = new GameField(new Dimension(640, 420));
 	}
 
 	@Override
 	public void render(Graphics2D arg0) {
-		// TODO Auto-generated method stub
 
+		// Рендерим последовательно фон и все игровые объекты
+		background.render(arg0);
+		spritesBalls.render(arg0);
+		spritesBricks.render(arg0);
+		spritesPaddles.render(arg0);
+		
+		// TODO: Рендер кол-ва очков, другой инофрмации (сейчас игра на весь экран)
 	}
 
 	@Override
 	public void update(long arg0) {
-		// TODO Auto-generated method stub
-
+		
+		// Апдейтим всё
+		spritesBalls.update(arg0);
+		spritesBricks.update(arg0);
+		spritesPaddles.update(arg0);
+		
+		// TODO: Проверка всех коллизий
+		// TODO: Апдейт событий пользовательского ввода
 	}
 
 }
