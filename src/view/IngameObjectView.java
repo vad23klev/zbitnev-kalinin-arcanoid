c4778351b08ce17cd9def726a613d88461e477f2
Gregory Zbitnev <zbitnev@hotmail.com>
2015-01-16 17:40:46 +0300

    Добавлено и реализовано добавление/удаление слушателей в IngameObjectView

--- src/view/IngameObjectView.java
+++ src/view/IngameObjectView.java
@@ -1,7 +1,9 @@
 ﻿package view;
 
 import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Float;
+import java.util.ArrayList;
 
 import model.Speed2D;
 import model.interaction.GenericEventListener;
@@ -16,28 +18,47 @@ import model.interaction.SpeedChangeListener;
 public class IngameObjectView
 		implements PositionChangeListener, SpeedChangeListener {
 
+	private ArrayList<PublishingSprite> sprites = new ArrayList<>();
+	private Point2D.Float position = null;
+	private ArrayList<PositionChangeListener> positionListeners;
+	private ArrayList<SpeedChangeListener> speedListeners;
+	
     /**
      * Необходимо использовать вместо прямого обращения к спрайту.
      * @param timeElapsed Прошедшее время.
      */
     public void update(int timeElapsed) {
         
+    	for (PublishingSprite s : sprites) {
+    		s.update(timeElapsed);
+    	}
+    	
+    	// TODO Сообщаем об изменениях слушателям (модели, то бишь)
     }
     
     public void render(Graphics2D g) {
     	
+    	for (PublishingSprite s : sprites) {
+    		s.render(g);
+    	}
     }
     
 	@Override
-	public void positionChanged(Float newpos) {
-		// TODO Auto-generated method stub
+	public void positionChanged(Point2D.Float newpos) {
 		
+		for (PublishingSprite s : sprites) {
+			
+			// TODO Здесь всё будет далеко не так тривиально
+			s.setLocation(newpos.x, newpos.y);
+    	}
 	}
 
 	@Override
 	public void speedChanged(Speed2D newspeed) {
-		// TODO Auto-generated method stub
 		
+		for (PublishingSprite s : sprites) {
+    		s.setSpeed(newspeed.x(), newspeed.y());
+    	}
 	}
 	
 	/**
@@ -46,6 +67,12 @@ public class IngameObjectView
 	 */
 	public void addPublishingSprite(PublishingSprite sprite) {
 		
+		if (sprite == null) {
+			// TODO Выброс исключения
+			return;
+		}
+		
+		sprites.add(sprite);
 	}
 	
 	/**
@@ -54,5 +81,38 @@ public class IngameObjectView
 	 */
 	public void removePublishingSprite(PublishingSprite sprite) {
 		
+		sprites.remove(sprite);
+	}
+	
+	/**
+	 * Добавить слушателя изменения позиции представления объекта
+	 * @param l Новый слушатель
+	 */
+	public void addPositionChangeListener(PositionChangeListener l) {
+		positionListeners.add(l);
+	}
+	
+	/**
+	 * Удалить слушателя изменения позиции представления объекта
+	 * @param l Удаляемый слушатель
+	 */
+	public void removePositionChangeListener(PositionChangeListener l) {
+		positionListeners.remove(l);
+	}
+	
+	/**
+	 * Добавить слушателя изменения скорости представления объекта
+	 * @param l Новый слушатель
+	 */
+	public void addSpeedChangeListener(SpeedChangeListener l) {
+		speedListeners.add(l);
+	}
+	
+	/**
+	 * Удалить слушателя изменения скорости представления объекта
+	 * @param l Удаляемый слушатель
+	 */
+	public void removeSpeedChangeListener(SpeedChangeListener l) {
+		speedListeners.remove(l);
 	}
 }
