4fc5566a582982e9db82af74ddae2c79de924098
Nikita Kalinin <nixorv@gmail.com>
2015-01-17 02:13:22 +0300

    Добавлена обработка групп спрайтов в предст. поля.
    Представление поля само добавляет и удаляет спрайт представления объекта из/в нужные группы спрайтов.
    
    Исправлены переводы строк.

--- src/view/GameFieldView.java
+++ src/view/GameFieldView.java
@@ -4,9 +4,12 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 import model.IngameObject;
+import model.ball.Ball;
 import model.ball.BasicBall;
 import model.brick.BreakableBrick;
+import model.brick.Brick;
 import model.interaction.GenericEventListener;
+import model.paddle.Paddle;
 
 import com.golden.gamedev.object.PlayField;
 import com.golden.gamedev.object.SpriteGroup;
@@ -17,62 +20,95 @@ import com.golden.gamedev.object.SpriteGroup;
  * @author Gregory Zbitnev <zbitnev@hotmail.com>
  *
  */
-public class GameFieldView extends PlayField implements GenericEventListener {
-
-	// Загруженные изображения для игровых объектов
-	public BufferedImage basicBallImg;
-	public BufferedImage breakableBrickImg;
+public class GameFieldView extends PlayField {
 	
 	// Игровые объекты
-	ArrayList<IngameObjectView> objectViews;
+	ArrayList<IngameObjectView> _objectViews = new ArrayList<>();
 	
 	public GameFieldView() {
-		basicBallImg = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
-		breakableBrickImg = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
 		
 		SpriteGroup balls = new SpriteGroup("balls");
 		SpriteGroup bricks = new SpriteGroup("bricks");
+		SpriteGroup paddles = new SpriteGroup("paddles");
 		this.addGroup(balls);
 		this.addGroup(bricks);
+		this.addGroup(paddles);
 	}
 
 	@Override
-	public void created(IngameObject obj) {
-		
-		// TODO Адекватное проектное решение для генерации представления объекта
-		if (obj instanceof BasicBall) {
-			IngameObjectView newobj = new IngameObjectView();
-			
-			PublishingSprite sprite = new PublishingSprite();
-			sprite.setImage(basicBallImg);
-			sprite.setLocation(obj.getPosition().x, obj.getPosition().y);
-			sprite.setObjectView(newobj);
-			newobj.addPublishingSprite(sprite);
-			
-			this.getGroup("balls").add(sprite);
-			
-			obj.addSpeedChangeListener(newobj);
-			obj.addPositionChangeListener(newobj);
-		}
-		else if (obj instanceof BreakableBrick) {
-			IngameObjectView newobj = new IngameObjectView();
-			
-			PublishingSprite sprite = new PublishingSprite();
-			sprite.setImage(breakableBrickImg);
-			sprite.setLocation(obj.getPosition().x, obj.getPosition().y);
-			sprite.setObjectView(newobj);
-			newobj.addPublishingSprite(sprite);
-			
-			this.getGroup("bricks").add(sprite);
-
-			obj.addSpeedChangeListener(newobj);
-			obj.addPositionChangeListener(newobj);
-		}
+	public void update(long timeElapsed) {
+	    
+	    super.update(timeElapsed);
+	    for (IngameObjectView ov : _objectViews) {
+	        ov.update(timeElapsed);
+	    }
 	}
-
-	@Override
-	public void destroyed(IngameObject obj) {
-		// TODO Auto-generated method stub
-		
+	
+	/**
+	 * Возвращает группу спрайтов мячей.
+	 * @return Группа спрайтов.
+	 */
+	public SpriteGroup getBallsGroup() {
+	    
+	    return this.getGroup("balls");
+	}
+	
+	/**
+	 * Возвращает группу спрайтов кирпичей.
+	 * @return Группа спрайтов.
+	 */
+	public SpriteGroup getBricksGroup() {
+	    
+	    return this.getGroup("bricks");
+	}
+	
+	/**
+	 * Возвращает группу спрайтов ракеток.
+	 * @return Группа спрайтов.
+	 */
+	public SpriteGroup getPaddlesGroup() {
+	    
+	    return this.getGroup("paddles");
+	}
+	
+	/**
+	 * Добавляет представление объекта на это поле. Этот метод добавляет объект в соответствующую группу спрайтов.
+	 * @param ov Представление.
+	 */
+	public void addObjectView(IngameObjectView ov) {
+	    
+	    _objectViews.add(ov);
+	    if (ov.getIngameObject() instanceof Ball) {
+	        getBallsGroup().add(ov.getSprite());
+	    } else if (ov.getIngameObject() instanceof Brick) {
+	        getBricksGroup().add(ov.getSprite());
+	    } else if (ov.getIngameObject() instanceof Paddle) {
+	        getPaddlesGroup().add(ov.getSprite());
+	    }
+	}
+	
+	/**
+	 * Удаляет представление объекта с этого представления поля и из группы спрайтов.
+	 * @param ov Представление.
+	 */
+	public void removeObjectView(IngameObjectView ov) {
+	    
+	    _objectViews.remove(ov);
+	    if (ov.getIngameObject() instanceof Ball) {
+            getBallsGroup().remove(ov.getSprite());
+        } else if (ov.getIngameObject() instanceof Brick) {
+            getBricksGroup().remove(ov.getSprite());
+        } else if (ov.getIngameObject() instanceof Paddle) {
+            getPaddlesGroup().remove(ov.getSprite());
+        }
+	}
+	
+	/**
+	 * Возвращает список представлений объектов на этом поле.
+	 * @return Список.
+	 */
+	public ArrayList<IngameObjectView> getObjectViews() {
+	    
+	    return (ArrayList<IngameObjectView>) _objectViews.clone();
 	}
 }
