bca4fda7cab528c271a26307936de9e8df929f63
Gregory Zbitnev <zbitnev@hotmail.com>
2015-01-16 16:13:35 +0300

    Добавлена жалкая реализация Speed2D. В дальшейшем будет сильно улучшена

--- src/model/Speed2D.java
+++ src/model/Speed2D.java
@@ -7,4 +7,24 @@ package model;
  */
 public class Speed2D {
 
+	private double x;
+	private double y;
+	
+	/**
+	 * Создаёт экземпляр вектора скорости на декартовой плоскости
+	 * @param xspeed Компонента вектора по x
+	 * @param yspeed Компонента вектора по y
+	 */
+	public Speed2D(double xspeed, double yspeed) {
+		x = xspeed;
+		y = yspeed;
+	}
+	
+	public double x() {
+		return x;
+	}
+	
+	public double y() {
+		return y;
+	}
 }
