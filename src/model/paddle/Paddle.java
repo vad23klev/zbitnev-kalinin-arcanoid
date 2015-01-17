8795af484d9425e91df5ec02b90a6aeaaa4f2b88
Nikita Kalinin <nixorv@gmail.com>
2015-01-17 15:51:58 +0300

    Реализован расчет вектора скорости при запуске шарика с ракетки

--- src/model/paddle/Paddle.java
+++ src/model/paddle/Paddle.java
@@ -80,27 +80,51 @@ public abstract class Paddle extends IngameObject {
     }
     
     /**
+     * Возвращает скорость мяча при запуске его с ракетки или отскока от ракетки.
+     * @param b Мяч.
+     * @return Вектор скорости.
+     */
+    public Speed2D getFireSpeed(Ball ball) {
+        
+        Point2D.Float paddleCenter  = new Point2D.Float(this.position.x + this.size.width / 2, this.position.y);
+        
+        // Относительные координаты центра мяча в декартовой системе координат (точка B).
+        // Считаем, что paddleCenter - это точка A(0, 0).
+        Point2D.Float relBallCenter = new Point2D.Float(ball.getPosition().x - paddleCenter.x,
+                paddleCenter.y - ball.getPosition().y - ball.getSize().height / 2);
+        
+        // Коэффициенты уравнения точки пересечения прямой и окружности.
+        double a = (Math.pow(relBallCenter.x, 2) + Math.pow(relBallCenter.y, 2)) / Math.pow(relBallCenter.x, 2);
+        double b = 0;
+        double c = -Math.pow(ball.getDefaultSpeedScalar(), 2);
+        
+        // Дискриминант.
+        double D = Math.pow(b, 2) - 4*a*c;
+        
+        // Точки пересечения.
+        Point2D.Float p1 = new Point2D.Float((float) ((-b + Math.sqrt(D)) / (2*a)), 0);
+        Point2D.Float p2 = new Point2D.Float((float) ((-b - Math.sqrt(D)) / (2*a)), 0);
+        
+        // Находим y{1,2} у точек.
+        p1.y = p1.x * relBallCenter.y / relBallCenter.x;
+        p2.y = p2.x * relBallCenter.y / relBallCenter.x;
+        
+        // Нужная точка пересечения имеет положительную y-координату.
+        Point2D.Float p = p1.y > 0 ? p1 : p2;
+        
+        // Находим горизонтальную и вертикальную сооставляющие вектора скорости.
+        // y отрицательный, чтобы перейти в экранную систему координат.
+        return new Speed2D(Math.abs(p.x), -p.y);
+    }
+    
+    /**
      * Запускает шары с ракетки.
      */
     public void fireBalls() {
         
-        //float paddleCenterX = this.position.x + this.size.width / 2;
-        
         while (!_balls.isEmpty()) {
             Ball b = _balls.get(0);
-            
-            /*float ballSpeed = b.getDefaultSpeedScalar();
-            float ballCenterX = b.getPosition().x + b.getSize().width / 2;
-            
-            float ballSpeedX = Math.abs(paddleCenterX - ballCenterX);
-            float ballSpeedY = (float)Math.sqrt(ballSpeed*ballSpeed - ballSpeedX*ballSpeedX);
-            
-            System.out.println(ballSpeedX);
-            System.out.println(ballSpeedY);
-            
-            b.setSpeed(new Speed2D(ballSpeedX, ballSpeedY));*/
-            b.setSpeed(new Speed2D(-0.2, -0.1)); // TODO Нормальный расчет вектора скорости.
-            
+            b.setSpeed(getFireSpeed(b));
             _balls.remove(b);
         }
     }
