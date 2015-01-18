2495fbc3c278144c5a36f8b9bb15a887b026c277
Gregory Zbitnev <zbitnev@hotmail.com>
2015-01-18 22:12:58 +0300

    Реализовано поведение при столкновении шаров

--- src/model/collision/BehaviourRebound.java
+++ src/model/collision/BehaviourRebound.java
@@ -9,6 +9,8 @@ import model.ball.Ball;
 import model.brick.Brick;
 import model.paddle.Paddle;
 
+import math.geom2d.Vector2D;
+
 /**
  * Поведение упрогого отскока при столкновении.
  * @author Nikita Kalinin <nixorv@gmail.com>
@@ -85,6 +87,44 @@ public class BehaviourRebound extends CollisionBehaviour {
 		}
 		else if (from instanceof Ball && to instanceof Ball) {
 			
+			Ball act = (Ball)from;
+			Ball pass = (Ball)to;
+			
+			// Вычисляется точка столкновения
+			float colx = (act.getCenter().x * pass.getRadius() 
+					      + pass.getCenter().x * act.getRadius()) 
+					   / (act.getRadius() + pass.getRadius());
+			float coly = (act.getCenter().y * pass.getRadius() 
+				      + pass.getCenter().y * act.getRadius()) 
+				   / (act.getRadius() + pass.getRadius());
+			
+			System.out.println("COLLISION! At point " + colx + "; " + coly);
+			System.out.println("\tactive ball is " + act.getCenter());
+			System.out.println("\tpassive ball is " + pass.getCenter());
+			
+			// Пассивный объект "отодвигается" по линии столкновения (линия, соединяющая центры 
+			// шаров) во избежание повторной коллизии
+			Point2D.Float movevect = new Point2D.Float(pass.getCenter().x - colx,
+													   pass.getCenter().y - coly);
+			Point2D.Float newpos = new Point2D.Float(pass.getCenter().x + movevect.x,
+													 pass.getCenter().y + movevect.y);
+			pass.setCenter(newpos);
+			
+			// Вычисляется новая скорость для пассивного объекта
+			Vector2D aspeed = new Vector2D(act.getSpeed().x(), act.getSpeed().y());
+			Vector2D pspeed = new Vector2D(pass.getSpeed().x(), pass.getSpeed().y());
+			Vector2D acenter = new Vector2D(act.getCenter().x, act.getCenter().y);
+			Vector2D pcenter = new Vector2D(pass.getCenter().x, pass.getCenter().y);
+			System.out.println("\tactive speed is " + aspeed.toString());
+			System.out.println("\tpassive speed before is " + pspeed.toString());
+			Vector2D newPSpeed = pspeed;
+			Vector2D pminusa = pcenter.minus(acenter);
+			newPSpeed = newPSpeed.minus(pminusa.times(
+					pspeed.minus(aspeed).dot(pminusa) / Math.pow(pminusa.norm(), 2.0)));
+			System.out.println("\tpassive speed after is " + newPSpeed.toString());
+			
+			// Новая скорость назначается пассивному объекту
+			pass.setSpeed(new Speed2D(newPSpeed.x(), newPSpeed.y()));
 		}
 	}
 }
