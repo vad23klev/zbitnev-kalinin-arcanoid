d5d5c147451df79c6f5c9aee8d34360e3fa4a34c
vad23klev <vad23klev@gmail.com>
2015-04-16 23:52:21 +0300

    В класс PublishingCollisionManager добвлена ссылка на стандартый менеджер столкновений, а так же его set'ер.

--- src/model/collision/CollisionManagers/PublishingCollisionBoundsManager.java
+++ src/model/collision/CollisionManagers/PublishingCollisionBoundsManager.java
@@ -2,19 +2,28 @@ package model.collision.CollisionManagers;
 
 import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.collision.CollisionBounds;
+import view.SpriteGTGE;
 
 /**
  * Менеджер столкновений игровых объектов с границами игрового поля.
  * @author Tenk0Kugen
  */
 public class PublishingCollisionBoundsManager extends CollisionBounds{
+    protected ModelCollisionManager _modelmanager;
 
     public PublishingCollisionBoundsManager(int i, int i1, int i2, int i3) {
         super(i, i1, i2, i3);
     }
 
     @Override
-    public void collided(Sprite sprite) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void collided(Sprite sprite) {   
+        _modelmanager.boundsCollisionOcured(((SpriteGTGE)sprite).getSpriteStorage().getObjectView().getIngameObject());
+    }
+    
+    public void setModelCollisionManager(ModelCollisionManager manager) {
+        if (manager == null) {
+                throw new NullPointerException();
+        }
+        _modelmanager = manager;
     }
 }
