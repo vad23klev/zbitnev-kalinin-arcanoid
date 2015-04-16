e0a6746d18392d4fd248e79abb4c0a060972262b
vad23klev <vad23klev@gmail.com>
2015-04-17 00:01:50 +0300

    Добавлены абстрактные методы в интерфейс SpriteStorage

--- src/view/SpriteStorage.java
+++ src/view/SpriteStorage.java
@@ -15,7 +15,9 @@ import model.Speed2D;
 public interface SpriteStorage {
     public Speed2D getSpeed();
     public void setSpeed(Speed2D speed);
-    public Point2D.Float getPosition();
-    public void setPosition (Point2D.Float position);
+    public Point2D.Double getPosition();
+    public void setPosition (Point2D.Double position);
+    public IngameObjectView getObjectView();
+    public void setObjectView(IngameObjectView view);
     
 }
