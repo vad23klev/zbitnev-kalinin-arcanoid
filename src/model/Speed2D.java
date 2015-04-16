4749afb0b275779e5e4c118c6f753bdf39c0111c
vad23klev <vad23klev@gmail.com>
2015-04-16 23:45:16 +0300

    Класс Speed2D теперь не изменяемый

--- src/model/Speed2D.java
+++ src/model/Speed2D.java
@@ -7,8 +7,8 @@ package model;
  */
 public class Speed2D implements Cloneable {
 
-	private double _x;
-	private double _y;
+	private final double _x;
+	private final double _y;
 	
 	/**
 	 * Создаёт экземпляр вектора скорости на декартовой плоскости
