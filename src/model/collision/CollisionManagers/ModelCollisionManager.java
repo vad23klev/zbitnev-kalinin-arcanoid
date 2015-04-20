c3fd2c0b2348db78ea03ce4ae52806909d1c325a
vad23klev <vad23klev@gmail.com>
2015-04-21 00:11:31 +0300

    Добавлена реализация менеджера коллизий для модели.

--- src/model/collision/CollisionManagers/ModelCollisionManager.java
+++ src/model/collision/CollisionManagers/ModelCollisionManager.java
@@ -1,7 +1,13 @@
 package model.collision.CollisionManagers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import model.Direction;
 import model.IngameObject;
+import model.collision.CollidedObject;
+import model.collision.SpecialBehaviours;
 
 /**
  * Содержит поведение объектов при столкновении.
@@ -10,11 +16,25 @@ import model.IngameObject;
 public class ModelCollisionManager {
     
     void collisionOcured(HashMap storage){
-        //TODO
+        Iterator i = storage.entrySet().iterator();
+	while (i.hasNext()) {
+            Map.Entry<CollidedObject, ArrayList<CollidedObject>> entry = (Map.Entry)i.next();
+            ArrayList<CollidedObject> others = entry.getValue();
+            CollidedObject current  = entry.getKey();
+            Iterator j = others.iterator();
+            while (j.hasNext()) {
+                CollidedObject other  = (CollidedObject)j.next();
+                current.object().processCollision(current, other);
+            }
+	}
     }
     
-    void boundsCollisionOcured(IngameObject obj){
-        
+    void boundsCollisionOcured(IngameObject obj, Direction direction){
+        if (direction.equals(Direction.east()) || direction.equals(Direction.west())) {
+            obj.setSpeed(obj.getSpeed().flipHorizontal());
+        } else {
+            obj.setSpeed(obj.getSpeed().flipVertical());
+        }
     }
     
     private HashMap DeepCopyStorage(HashMap storage){
