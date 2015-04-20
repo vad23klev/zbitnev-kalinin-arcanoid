cf6bef36099648a78c07e78dc21b7dffe911ddb6
vad23klev <vad23klev@gmail.com>
2015-04-21 00:09:42 +0300

    Реализована работа менеджера коллизий внутриигровых объектов.

--- src/model/collision/CollisionManagers/PublishingCollisionManager.java
+++ src/model/collision/CollisionManagers/PublishingCollisionManager.java
@@ -19,68 +19,75 @@ import com.golden.gamedev.object.collision.CollisionShape;
  *
  */
 public class PublishingCollisionManager extends AdvanceCollisionGroup {
+    protected ModelCollisionManager _modelmanager;
 
-	protected HashMap<CollidedObject, ArrayList<CollidedObject>> _storage = new HashMap<>();
-	
-	@Override
-	public void collided(Sprite arg0, Sprite arg1) {
-		// TODO Auto-generated method stub
+    protected HashMap<CollidedObject, ArrayList<CollidedObject>> _storage = new HashMap<>();
 
-	}
+    @Override
+    public void collided(Sprite arg0, Sprite arg1) {
+            // TODO Auto-generated method stub
 
-	@Override
-	public boolean isCollide(Sprite s1, Sprite s2, CollisionShape shape1, CollisionShape shape2) {
-		
-		boolean retval = super.isCollide(s1, s2, shape1, shape2);
-		
-		// Словарь столкновений будет формироваться в процессе детекции коллизий
-		if (retval) {
-		
-			int obj1colside = -1, obj2colside = -1;
-			switch (this.collisionSide) {
-			case CollisionGroup.BOTTOM_TOP_COLLISION:
-				obj1colside = CollidedObject.SIDE_TOP;
-				obj2colside = CollidedObject.SIDE_BOTTOM;
-				break;
-			case CollisionGroup.TOP_BOTTOM_COLLISION:
-				obj1colside = CollidedObject.SIDE_BOTTOM;
-				obj2colside = CollidedObject.SIDE_TOP;
-				break;
-			case CollisionGroup.RIGHT_LEFT_COLLISION:
-				obj1colside = CollidedObject.SIDE_LEFT;
-				obj2colside = CollidedObject.SIDE_RIGHT;
-				break;
-			case CollisionGroup.LEFT_RIGHT_COLLISION:
-				obj1colside = CollidedObject.SIDE_RIGHT;
-				obj2colside = CollidedObject.SIDE_LEFT;
-				break;
-			default:
-				break;
-			}
-			
-			CollidedObject obj1 = new CollidedObject(
-					((SpriteGTGE)s1).getSpriteStorage().getObjectView().getIngameObject(), 
-					new Point2D.Double((float)s1.getOldX(), (float)s1.getOldY()),
-					obj1colside, new Rectangle2D.Double(shape1.getX(), 
-													    shape1.getY(), 
-													    shape1.getWidth(), 
-													    shape1.getHeight()));
-			
-			CollidedObject obj2 = new CollidedObject(
-					((SpriteGTGE)s2).getSpriteStorage().getObjectView().getIngameObject(), 
-					new Point2D.Double((float)s2.getOldX(), (float)s2.getOldY()),
-					obj2colside, new Rectangle2D.Double(shape2.getX(), 
-						    							shape2.getY(), 
-						    							shape2.getWidth(), 
-						    							shape2.getHeight()));
-			
-			if (!_storage.keySet().contains(obj1)) {
-				_storage.put(obj1, new ArrayList<CollidedObject>());
-			}
-			_storage.get(obj1).add(obj2);
-		}
-		
-		return retval;
+    }
+
+    public PublishingCollisionManager(ModelCollisionManager manager) {
+        if (manager == null) {
+                throw new NullPointerException();
+        }
+        _modelmanager = manager;
+    }
+    @Override
+    public boolean isCollide(Sprite s1, Sprite s2, CollisionShape shape1, CollisionShape shape2) {
+
+            boolean retval = super.isCollide(s1, s2, shape1, shape2);
+
+            // Словарь столкновений будет формироваться в процессе детекции коллизий
+            if (retval) {
+
+                    int obj1colside = -1, obj2colside = -1;
+                    switch (this.collisionSide) {
+                    case CollisionGroup.BOTTOM_TOP_COLLISION:
+                            obj1colside = CollidedObject.SIDE_TOP;
+                            obj2colside = CollidedObject.SIDE_BOTTOM;
+                            break;
+                    case CollisionGroup.TOP_BOTTOM_COLLISION:
+                            obj1colside = CollidedObject.SIDE_BOTTOM;
+                            obj2colside = CollidedObject.SIDE_TOP;
+                            break;
+                    case CollisionGroup.RIGHT_LEFT_COLLISION:
+                            obj1colside = CollidedObject.SIDE_LEFT;
+                            obj2colside = CollidedObject.SIDE_RIGHT;
+                            break;
+                    case CollisionGroup.LEFT_RIGHT_COLLISION:
+                            obj1colside = CollidedObject.SIDE_RIGHT;
+                            obj2colside = CollidedObject.SIDE_LEFT;
+                            break;
+                    default:
+                            break;
+                    }
+
+                    CollidedObject obj1 = new CollidedObject(
+                                    ((SpriteGTGE)s1).getSpriteStorage().getObjectView().getIngameObject(),
+                                    new Point2D.Double((float)s1.getOldX(), (float)s1.getOldY()),
+                                    obj1colside, new Rectangle2D.Double(shape1.getX(),
+                                                                                                        shape1.getY(),
+                                                                                                        shape1.getWidth(),
+                                                                                                        shape1.getHeight()));
+
+                    CollidedObject obj2 = new CollidedObject(
+                                    ((SpriteGTGE)s2).getSpriteStorage().getObjectView().getIngameObject(),
+                                    new Point2D.Double((float)s2.getOldX(), (float)s2.getOldY()),
+                                    obj2colside, new Rectangle2D.Double(shape2.getX(),
+                                                                                                    shape2.getY(),
+                                                                                                    shape2.getWidth(),
+                                                                                                    shape2.getHeight()));
+
+                    if (!_storage.keySet().contains(obj1)) {
+                            _storage.put(obj1, new ArrayList<CollidedObject>());
+                    }
+                    _storage.get(obj1).add(obj2);
+            }
+            _modelmanager.collisionOcured(_storage);
+            return retval;
 	}
 	
 	/**
