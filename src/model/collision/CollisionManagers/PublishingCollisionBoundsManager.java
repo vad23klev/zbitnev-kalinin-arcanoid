92519acd1ec68403669944200ce6307b692f55ac
Vadim Klevtsov <vad23klev@gmail.com>
2015-05-29 01:57:00 +0300

    Исправлен метод collided.PublishingCollisionBoundsManager.

--- src/model/collision/CollisionManagers/PublishingCollisionBoundsManager.java
+++ src/model/collision/CollisionManagers/PublishingCollisionBoundsManager.java
@@ -3,7 +3,10 @@ package model.collision.CollisionManagers;
 import com.golden.gamedev.object.Background;
 import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.collision.CollisionBounds;
+import java.awt.Shape;
+import java.awt.geom.Rectangle2D;
 import model.Direction;
+import model.collision.CollidedObject;
 import view.SpriteGTGE;
 
 /**
@@ -23,22 +26,16 @@ public class PublishingCollisionBoundsManager extends CollisionBounds{
 
     @Override
     public void collided(Sprite sprite) {   
-        Direction direction;
-        if (isCollisionSide(RIGHT_COLLISION)) {
-            direction = Direction.east();
-            _modelmanager.boundsCollisionOcured(((SpriteGTGE)sprite).getSpriteStorage().getObjectView().getIngameObject(), direction);
-        }
-        if (isCollisionSide(TOP_COLLISION)) {
-            direction = Direction.north();
-            _modelmanager.boundsCollisionOcured(((SpriteGTGE)sprite).getSpriteStorage().getObjectView().getIngameObject(), direction);
-        }
-        if (isCollisionSide(LEFT_COLLISION)) {
-            direction = Direction.west();
-            _modelmanager.boundsCollisionOcured(((SpriteGTGE)sprite).getSpriteStorage().getObjectView().getIngameObject(), direction);
-        }
-        if (isCollisionSide(BOTTOM_COLLISION)){
-            direction = Direction.south();
-            _modelmanager.boundsCollisionOcured(((SpriteGTGE)sprite).getSpriteStorage().getObjectView().getIngameObject(), direction);
-        }
+        int collisionSide = -1;
+        
+        collisionSide = isCollisionSide(RIGHT_COLLISION)? CollidedObject.SIDE_RIGHT: collisionSide;
+        collisionSide = isCollisionSide(LEFT_COLLISION)? CollidedObject.SIDE_LEFT: collisionSide;
+        collisionSide = isCollisionSide(BOTTOM_COLLISION)? CollidedObject.SIDE_BOTTOM: collisionSide;
+        collisionSide = isCollisionSide(TOP_COLLISION)? CollidedObject.SIDE_TOP: collisionSide;
+        _modelmanager.boundsCollisionOcured(((SpriteGTGE)sprite).getSpriteStorage().getObjectView().getIngameObject(), 
+                collisionSide ,new Rectangle2D.Double(getCollisionShape1(sprite).getX(),
+                                                        getCollisionShape1(sprite).getY(),
+                                                        getCollisionShape1(sprite).getWidth(),
+                                                        getCollisionShape1(sprite).getHeight()));
     }
 }
