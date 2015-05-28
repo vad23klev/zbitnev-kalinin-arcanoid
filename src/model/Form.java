8a89a3b8db3922e775e4d416bb2d360a87f70f46
Vadim Klevtsov <vad23klev@gmail.com>
2015-05-28 23:15:15 +0300

    Класс формы стал абстрактным.

--- src/model/Form.java
+++ src/model/Form.java
@@ -4,16 +4,6 @@ package model;
  * Контейнер форм и размеров.
  * @author Tenk0Kugen
  */
-public class Form {
+public abstract class Form {
     
-    private String _type;
-    
-    	/**
-	 * Возвращает поле, на котором находится объект.
-	 * @return Игровое поле.
-	 */
-	public String getType() {
-	    
-	    return _type;
-	}
 }
